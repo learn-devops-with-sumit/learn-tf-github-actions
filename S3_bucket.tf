f9efc49e48b7a4691f2936c5365ba506d11e7b5b
gourav615 <ggouravsharma674@gmail.com>
2025-01-31 17:28:35 +0530

    create new s3_bucket

--- S3_bucket.tf
+++ S3_bucket.tf
@@ -1,41 +1,15 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.82.2"
-    }
-    
-    random = {
-      source = "hashicorp/random"
-      version = "3.6.3"
-    }
+provider "aws" {
+  region = "ap-south-1" 
+}
+
+resource "aws_s3_bucket" "Github_Action" {
+  bucket = "Git_S3_bucket_90500"
+
+  tags = {
+    Name        = "MyS3Bucket"
   }
 }
 
-provider "aws" {
-    region = "ap-south-1"
-
-  
-}
-
-resource "random_id" "ran_id" {
-    byte_length = 8
-  
-}
-
-resource "aws_s3_bucket" "demo-bucket" {
-    bucket = "demo-bucket-${random_id.ran_id.hex}"
-  
-}
-
-resource "aws_s3_object" "bucket-dta" {
-    bucket = aws_s3_bucket.demo-bucket.bucket
-    source = "my.txt"
-    key = "mydata.txt"
-  
-}
-
-output "out" {
-    value = random_id.ran_id.hex
-  
+output "s3_bucket_name" {
+  value = aws_s3_bucket.Github_Action.bucket
 }
